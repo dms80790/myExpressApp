8992c2080bd128640aa65c5f596ca63089712953
David <dms80790@gmail.com>
2021-08-03 20:33:49 -0400

    add user and password inputs

--- shareReviewApp/routes/gaming.js
+++ shareReviewApp/routes/gaming.js
@@ -8,22 +8,22 @@ router.get('/', function(req, res, next) {
 
 /* GET request for playstation homepage */
 router.get('/playstation', function(req, res, next) {
-  res.render('gaming_platform', { title: 'PlayStation' });
+  res.render('gaming_platform', { title: 'PlayStation Home' });
 });
 
 //GET request for xbox homepage
 router.get('/xbox', function(req, res, next) {
-  res.render('gaming_platform', { title: 'XBOX' });
+  res.render('gaming_platform', { title: 'XBOX Home' });
 });
 
-//POST request for nintendo homepage
+//GET request for nintendo homepage
 router.get('/nintendo', function(req, res, next) {
-  res.render('gaming_platform', { title: 'Nintendo' });
+  res.render('gaming_platform', { title: 'Nintendo Home' });
 });
 
 //GET request for pc homepage
 router.get('/pc', function(req, res, next) {
-  res.render('gaming_platform', { title: 'PC' });
+  res.render('gaming_platform', { title: 'PC Home' });
 });
 
 module.exports = router;
